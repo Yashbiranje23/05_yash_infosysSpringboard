9d8fece5139367ce23b214513e2b84f3a052f82d
Yashbiranje23 <126186151+Yashbiranje23@users.noreply.github.com>
2023-12-11 11:50:49 +0530

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,13 @@
+public class Account {
+    private double balance = 500000.00;
+    public double  getBalance(int accountId) {
+      // logic here
+      return balance;
+    }
+    public static void main(String[] args) {
+      Account accnt = new Account();
+      double value = accnt.getBalance(123456);
+      System.out.println("The balance is: " + value);
+    }
+  }
+  
